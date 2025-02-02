edbfd550f107a39efe6a14f36e7b0ba6651e4282
Abdullah Yahya <abdullah747@gmail.com>
2025-02-01 22:55:01 -0800

    updated Forrester image

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    theme: {
+      extend: {
+        colors: {
+          'primary': '#ff49db',
+        },
+        fontFamily: {
+          'sans': ['Helvetica', 'Arial', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+    content: ["./src/**/*.{njk,md,html}", "./src/**/*.svg",]
+  }
